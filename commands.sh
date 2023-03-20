7b3b8788606a2e72a01387d3d040b252806dcad8
m2rtenm <marten.mandla@gmail.com>
2023-03-20 17:26:14 +0200

    Makefile test fix, requirements adding

--- commands.sh
+++ commands.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 APP_NAME='nd082-marten'
-LOCATION='westeurope'
-SKU='FREE'
+LOCATION='westeurope' # closest location geographically
+SKU='FREE' # for cost effective purposes
 
 az webapp up -n $APP_NAME -l $LOCATION --sku $SKU
-
-./make_predict_azure_app.sh
-
-locust -f locustfile.py -H https://$APP_NAME.azurewebsites.net
