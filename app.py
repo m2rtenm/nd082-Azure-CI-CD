091c3a4e30a3d3f719baf103228e1b8e9d1f5d06
m2rtenm <marten.mandla@gmail.com>
2023-03-24 21:15:24 +0200

    Demo commit

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home. Welcome! Hi!</h3>"
+    html = "<h3>Sklearn Prediction Home. Welcome to the page!</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
